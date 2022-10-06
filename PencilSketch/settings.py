9b79b67e78802c75b039865ba77eb210da9cb706
patrinhoo <81069467+patrinhoo@users.noreply.github.com>
2022-10-06 13:34:25 +0200

    Staticfiles_dirs

--- PencilSketch/settings.py
+++ PencilSketch/settings.py
@@ -127,6 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
