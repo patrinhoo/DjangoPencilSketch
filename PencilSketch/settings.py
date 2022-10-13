abaa44f75d0fdfe99c151c12172ce29c559b090b
patrinhoo <81069467+patrinhoo@users.noreply.github.com>
2022-10-13 12:15:21 +0200

    bugs

--- PencilSketch/settings.py
+++ PencilSketch/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
